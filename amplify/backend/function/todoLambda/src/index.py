d158a9b4394077a418c5bdef2344b4c196d717c3
Vishal Bajoria <babynini@Vishals-MacBook-Pro.local>
2020-07-27 16:05:19 -0400

    adding backend

---
+++ amplify/backend/function/todoLambda/src/index.py
@@ -0,0 +1,7 @@
+def handler(event, context):
+  print('received event:')
+  print(event)
+  todos = [{'id':1, 'name':'Eat'}, {'id':2, 'name':'Sleep'}, {'id':3, 'name':'Code'}]
+  return {
+    todos
+  }
