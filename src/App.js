420e5337b703e32199847e7b4cccd66f6d9d5cf1
Vishal Bajoria <babynini@Vishals-MacBook-Pro.local>
2020-07-27 15:30:21 -0400

    first git add from mac to react-app

--- src/App.js
+++ src/App.js
@@ -2,25 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
+Amplify.configure(config);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
