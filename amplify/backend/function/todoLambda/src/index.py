644bd556693b68ebe0d7c86747dd21d130194658
Vishal Bajoria <babynini@Vishals-MacBook-Pro.local>
2020-07-27 16:22:50 -0400

    adding fixes

--- amplify/backend/function/todoLambda/src/index.py
+++ amplify/backend/function/todoLambda/src/index.py
@@ -2,6 +2,4 @@ def handler(event, context):
   print('received event:')
   print(event)
   todos = [{'id':1, 'name':'Eat'}, {'id':2, 'name':'Sleep'}, {'id':3, 'name':'Code'}]
-  return {
-    todos
-  }
+  return  todos
--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 import { withAuthenticator } from 'aws-amplify-react';
+import { API } from 'aws-amplify';
 
 Amplify.configure(config);
+
+
+
 function App() {
+  const [todos, setTodos] = useState(null)
+  async function getTodos(){
+    const apiData = await API.get('todos', '/items', {});
+    setTodos(apiData)
+  }
+
+  useEffect(() => {
+    getTodos()
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,7 +30,7 @@ function App() {
         <p>
           Hello World changed!
         </p>
-        
+        <p>{todos}</p>        
       </header>
     </div>
   );
